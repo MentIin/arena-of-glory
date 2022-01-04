c8bb095cdc901f5004a420c271d1ee619f0b3c5c
MentIin <miheevdeman776@gmail.com>
2022-01-04 13:09:20 +0200

    draw_text()

--- additional.py
+++ additional.py
@@ -99,3 +99,11 @@ def roulette(chance):
     if random() * 100 <= chance:
         return True
     return False
+
+
+def draw_text(surf, text, size, x, y, font_name=pygame.font.match_font('arial')):
+    font = pygame.font.Font(font_name, size)
+    text_surface = font.render(text, True, (255, 255, 255))
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (x, y)
+    surf.blit(text_surface, text_rect)
